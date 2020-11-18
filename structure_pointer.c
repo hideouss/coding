79dcb671681d50f9b2f24ebead373e5753649bdd
Your Name <you@example.com>
2020-11-18 03:39:17 -0800

    c

--- structure_pointer.c
+++ structure_pointer.c
@@ -3,15 +3,16 @@
 struct person{
 	char name[30];
 	int age;
-};
+}per, *ptrper;
 
 int main()
 {
-	struct person per;
-	struct person *ptrper;
+  struct person per = {"z", 24};
+//	struct person per;
+	//struct person *ptrper;
 	ptrper = &per;
-	scanf("%s", ptrper->name);
-  scanf("%d", &ptrper->age);
+	//scanf("%s", ptrper->name);
+  //scanf("%d", &ptrper->age);
 	printf("%s %d\n", ptrper->name,ptrper->age);
 
 	return 0;
